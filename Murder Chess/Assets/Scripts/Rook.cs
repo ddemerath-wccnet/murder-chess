6e6b4ddc7011501cdf5744eb6cd91d9f7980ea53
amargolisdias <amargolisdias@wccnet.edu>
2025-03-11 17:45:31 -0400

    Added Rook/Buy Menu
    Buy menu needs a buildout - thats what Im working on next. the assets are in, things just need to be hooked up. I'm going to put in back buttons in and perhaps add a relic menu/some new graphics for it.
    Rook moves as intended but isn't playing nice with the health bar system .

---
+++ Murder Chess/Assets/Scripts/Rook.cs
@@ -0,0 +1,117 @@
+using System.Runtime.InteropServices.WindowsRuntime;
+using NUnit.Framework;
+using UnityEngine;
+
+public class Rook : BasePiece
+{
+    public float changeDirectionTime = .5f;
+    private Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right };
+    private Vector3 currentDirection;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    protected override void Start()
+    {
+        SetRandomDirection();
+        InvokeRepeating(nameof(SetRandomDirection), changeDirectionTime, changeDirectionTime);
+        base.Start();
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        // if (duplicateTimer > 0) duplicateTimer -= GlobalVars.DeltaTimePiece;
+        // base.Update();
+        // transform.Translate(currentDirection * GlobalVars.DeltaTimePiece * PieceSpeed); 
+        base.Update();
+    }
+    void SetRandomDirection()
+    {
+        currentDirection = directions[Random.Range(0, directions.Length)];
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        SetRandomDirection();
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Player player = collision.gameObject.GetComponent<Player>();
+            player.DamagePlayer(HurtPlayerFor());
+        }
+        else if (collision.gameObject.tag != "Player")
+        {
+            Collider2D otherCollider = collision.gameObject.GetComponent<Collider2D>();
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), otherCollider);
+        }
+    }
+
+    public override Vector3 SelectTarget()
+    {
+        //Add randomness to movetimer (so pieces dont sync up and look bad)
+        PieceCycleTimer += Random.Range(-0.25f, 0.25f);
+
+        //select player as target
+        // return GlobalVars.player.transform.position;
+        return transform.position + currentDirection;
+    }
+
+    public override bool Move(Vector2 target, Vector2 distance, Vector2 moveDir, float moveTime, float moveTimerNormalized)
+    {
+        // if (Mathf.Abs(distance.magnitude) >= 1) // if piece is more than 1 unit away
+        // {
+        //     transform.position += GlobalVars.DeltaTimePiece * PieceSpeed * (Vector3)moveDir;
+        //     return false;  //continue moving
+        // }
+        // else
+        // {
+        //     return true; //stop moving and go to next stage
+        // }
+
+        transform.Translate(currentDirection * PieceSpeed * GlobalVars.DeltaTimePiece);
+        return false;
+    }
+
+    public override bool ShouldAttack()
+    {
+        // if (Mathf.Abs((GlobalVars.player.transform.position - transform.position).magnitude) <= 3) // if piece is more than 3 unit away
+        // {
+        //     attackTimer = 1.5f;
+        //     attackTarget = GlobalVars.player.transform.position;
+        //     return true;  //continue moving
+        // }
+        // else
+        // {
+        //     return false;
+        // }
+        return false;
+    }
+
+    float attackTimer;
+    Vector3 attackTarget;
+    public override bool Attack(out bool isDangerous)
+    {
+        // attackTimer -= GlobalVars.DeltaTimePiece;
+        // isDangerous = true; //Piece can hurt you while attacking
+        // Vector2 moveDir = (attackTarget - transform.position).normalized;
+        // transform.position += (Vector3)moveDir * PieceSpeed * 2 * GlobalVars.DeltaTimePiece;
+
+        // if (attackTimer >= 0) return false; //do attack for attacktimer sec
+        // else return true;
+        isDangerous = false;
+        return true;
+    }
+
+    public float duplicateTimer; //Duplicate cooldown
+    public int duplicateLimit = 3; //Duplicate limit
+    public override float HurtPlayerFor() //Extra code on top of Default Implementation
+    {
+        // if (duplicateTimer <= 0 && duplicateLimit > 0)
+        // {
+        //     duplicateTimer = 2;
+        //     duplicateLimit -= 1;
+        //     GameObject clone = GameObject.Instantiate(gameObject); //Duplicates when hurting player
+        //     clone.GetComponent<Pawn>().duplicateTimer = duplicateTimer;
+        //     clone.GetComponent<Pawn>().duplicateLimit = duplicateLimit;
+        //     clone.GetComponent<Pawn>().isDangerous = false;
+        // }
+        return base.HurtPlayerFor();
+    }
+}
