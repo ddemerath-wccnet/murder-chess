14fc02ae798317850525c42b4980b0527db1db78
amargolisdias <amargolisdias@wccnet.edu>
2025-03-31 18:03:03 -0400

    Beam Spell
    -Beam Spell/animation added
    -changed enemy prefab piece tags to "enemy"
    -Added shop script to beam spell
    (lets see if it works as intended)
    I will have to put enemy tag on knight again because this pull made me overwrite it for some reason

---
+++ Murder Chess/Assets/Scripts/CelestialBeamSpell.cs
@@ -0,0 +1,63 @@
+
+using System.Security.Cryptography;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CelestialBeamSpell : BaseSpell
+{
+    [SerializeField]
+    private float spellWidth;
+    [SerializeField]
+    private float spellLength;
+    [SerializeField]
+    private Vector2 spellOffset = new Vector2(4f, 0);
+    [SerializeField]
+    private float aoeDamage = 10f;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    protected override void SpellStart()
+    {
+
+
+        Debug.Log("SpellStart called");
+        Vector2 spellFront = (Vector2)transform.parent.position + spellOffset;
+        transform.position = spellFront;
+
+
+
+
+
+        // Transform cbsTransform = transform.Find("Celestial Beam Spell");
+        Animator animation = GameObject.Find("Celestial Beam Spell").GetComponent<Animator>();
+        GameObject.Find("Celestial Beam Spell");
+
+
+        if (animation != null)
+        {
+            Debug.Log("animator is is not null");
+            animation.SetTrigger("PlayBeamSpell");
+            Debug.Log("PlayBeamSpell trigger fired!");
+
+        }
+
+
+
+        Collider2D[] areaColliders = Physics2D.OverlapBoxAll(spellFront, new Vector2(spellLength, spellWidth), 0f);
+        foreach (var areaCollider in areaColliders)
+        {
+            if (areaCollider.gameObject.CompareTag("Enemy"))
+            {
+                Debug.Log("Enemy in range");
+                BasePiece basePiece = areaCollider.GetComponent<BasePiece>();
+                basePiece.DamagePiece(basePiece.PieceDamage * aoeDamage);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    protected override bool SpellUpdate()
+    {
+        return true;
+    }
+}
