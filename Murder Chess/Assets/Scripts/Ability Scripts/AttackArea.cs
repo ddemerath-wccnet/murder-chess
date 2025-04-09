4bb401bfafba0b2d64b0fca8d5a5055d15f91944
DonaldJackson04 <donniemjackson0101@gmail.com>
2025-04-09 16:45:09 -0400

    The bomb ability now damages and burns the player.

--- Murder Chess/Assets/Scripts/Ability Scripts/AttackArea.cs
+++ Murder Chess/Assets/Scripts/Ability Scripts/AttackArea.cs
@@ -5,28 +5,75 @@ public class AttackArea : MonoBehaviour
 {
     public List<BasePiece> collisionList = new List<BasePiece>();
     public float damageMulti;
-    public bool doesItBurn;
-    public float burnDuration;
+    public bool selfDamage;
+    public bool burns = false;
+    public float burnDuration = 0f;
+    public bool bleeds = false;
+    public float bleedDuration = 0f;
     private void start() {
 
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
+        Debug.LogWarning("Hit");
         BasePiece basePiece;
+        Player player;
         if (collision.TryGetComponent<BasePiece>(out basePiece))
         {
             if (!collisionList.Contains(basePiece))
             {
                 collisionList.Add(basePiece);
                 GlobalVars.player.GetComponent<Player>().HitPiece(basePiece, 0, damageMulti, 1f);
-                if(doesItBurn) {
+                if(burns) {
 
                     new BurnEffect(basePiece, burnDuration);
 
                 }
+                if(bleeds) {
+
+                    //bleed enemy
+
+                }
             }
         }
+
+
+        if (selfDamage && collision.TryGetComponent<Player>(out player)) 
+        {
+
+            Debug.LogWarning("Player Hit");
+
+            // GlobalVars.player.GetComponent<Player>().DamagePlayer(damageMulti);
+            if(burns) {
+
+                    new BurnEffect(GlobalVars.player.GetComponent<Player>(), burnDuration);
+
+                }
+                if(bleeds) {
+
+                    //bleed enemy
+
+            }
+
+            GlobalVars.player.GetComponent<Player>().DamagePlayer(damageMulti);
+
+        }
+        // if(selfDamage && collision.gameObject.CompareTage(Player)) 
+        // {
+        //     GlobalVars.player.GetComponent<Player>().DamagePlayer(damageMulti);
+        //     if(burns) {
+
+        //             new BurnEffect(GlobalVars.player.GetComponent<Player>(), burnDuration);
+
+        //         }
+        //         if(bleeds) {
+
+        //             //bleed enemy
+
+        //     }
+        // }
     }
 
 }
