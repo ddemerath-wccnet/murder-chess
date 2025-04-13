ad904b358fdb9f4a4debee2bdbcb68e3e2107316
ddemerath-wccnet <ddemerath@wccnet.edu>
2025-04-13 18:47:28 -0400

    wild magic fix

--- Murder Chess/Assets/Scripts/Spell Scripts/WildMagicSpell.cs
+++ Murder Chess/Assets/Scripts/Spell Scripts/WildMagicSpell.cs
@@ -16,14 +16,14 @@ public class WildMagicSpell : BaseSpell
         BaseSpell[] spellItemsTemp = Resources.LoadAll<BaseSpell>("Prefabs/Shop/Spells");
         spells = spellItemsTemp.ToList();
         numberOfSpells = spells.Count();
-
+        spells.Remove(Resources.LoadAll<WildMagicSpell>("Prefabs/Shop/Spells")[0]);
     }
 
     protected override void SpellStart()
     {
         Debug.Log("spell start");
         playerPosition = GlobalVars.player.transform.position;
-        spellCalled = Instantiate(spells[Random.Range(0 , (numberOfSpells))], playerPosition, transform.rotation, transform);
+        spellCalled = Instantiate(spells[Random.Range(0 , (numberOfSpells + 1))], playerPosition, transform.rotation, transform);
         //BaseSpell baseSpellScript = spellCalled.GetComponent<BaseSpell>();
         spellCalled.SpellCost = 0;
         spellCalled.CallActivate();
