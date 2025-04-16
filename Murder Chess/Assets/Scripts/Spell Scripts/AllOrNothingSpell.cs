5af6843677ef45b064593c9eb2743809d620dbb4
DonaldJackson04 <donniemjackson0101@gmail.com>
2025-04-16 16:57:12 -0400

    Added the AllOrNothingSpell
    I wanted to implement more "give and take" kind of spells / abilities.

---
+++ Murder Chess/Assets/Scripts/Spell Scripts/AllOrNothingSpell.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class AllOrNothingSpell : BaseSpell
+{
+
+    public float spellTimer;
+    public float playerDamageModifier;
+    public float pieceDamageModifier;
+    private float instanceSpellTimer;
+
+    protected override void SpellStart()
+    {
+        Debug.Log("spell start");
+        // GlobalVars.player.GetComponent<Player>().PlayerRegen += 0.5f;
+        new ModifierEntry("multiplier_PlayerDamage", playerDamageModifier, 10);
+        new ModifierEntry("multiplier_PieceDamage", pieceDamageModifier, 10);
+        instanceSpellTimer = spellTimer;
+    }
+
+    protected override bool SpellUpdate()
+    {
+        instanceSpellTimer -= GlobalVars.DeltaTimePlayer;
+
+        if (instanceSpellTimer < 0)
+        {
+            // GlobalVars.player.GetComponent<Player>().PlayerRegen -= 0.5f;
+            return true;
+        }
+        else return false;
+    }
+}
