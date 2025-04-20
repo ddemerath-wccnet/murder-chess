e8d03316e89aed10309355917b355d3fa679f772
Chris Watson <c.watson7@protonmail.com>
2025-04-20 10:36:39 -0400

    Added decoy / shockwave SFX and decoy sprite

--- Murder Chess/Assets/Scripts/Spell Scripts/ShockwaveSpell.cs
+++ Murder Chess/Assets/Scripts/Spell Scripts/ShockwaveSpell.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class ShockwaveSpell : BaseSpell
 {
     public GameObject shockwavePrefab;
+    public AudioSource sound;
 
     protected override void SpellStart()
     {
         Vector3 spawnOffset = new Vector3(0, -0.3f, 0); // 20 pixels = 0.2 units in Unity (assuming 100 pixels/unit)
         Instantiate(shockwavePrefab, GlobalVars.player.transform.position + spawnOffset, Quaternion.identity);
+        sound.Play();
         //Instantiate(shockwavePrefab, GlobalVars.player.transform.position, Quaternion.identity);
     }
 
